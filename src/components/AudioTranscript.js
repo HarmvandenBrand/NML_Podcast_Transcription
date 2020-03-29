c2a0b1466cc1e17b4ada3cbb9c7d5012cc87fed2
rusane <43827639+rusane@users.noreply.github.com>
2020-03-29 22:47:20 +0200

    Make basic transcript with audio player screen

---
+++ src/components/AudioTranscript.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { audio, transcript } from '../examplePodcast'; // example
+
+function TranscriptView(props) {
+	const { transcript } = props;
+
+	return (
+		<Container maxWidth={'md'} style={{ marginBottom: '8vh' }}>
+			<Typography component={'div'}>
+				{transcript}
+			</Typography>
+		</Container>
+	);
+}
+
+function AudioPlayer(props) {
+	const { audioSrc, audioRef } = props;
+
+	return (
+		<audio
+			controls
+			style={{ position: 'fixed', bottom: 0, width: "100%" }}
+			ref={audioRef}
+		>
+			<source src={audioSrc} type="audio/mpeg" />
+		</audio>
+	);
+}
+
+/**
+ * Maps paragraph tags around each paragraph of the transcript.
+ * For illustrative purposes, it also maps a span around the paragraph
+ * with dummy (meaningless) timestamps to make the paragraph clickable.
+ * 
+ * @param {string} transcript the podcast transcript
+ * @param {function} handleClick _onClick_ handler
+ */
+function mapParagraphTag(transcript, handleClick) {
+	let paragraphs = transcript.split(/\r?\n/).filter(Boolean);
+	// index as key is bad practice in general but as the order of 
+	// the paragraphs is fixed this is not an issue here
+	return paragraphs.map((p, idx) =>
+		<p key={idx}>
+			<span onClick={() => handleClick(idx)}>
+				{p}
+			</span>
+		</p>);
+}
+
+function AudioTranscript(props) {
+	const audioRef = React.useRef(null);
+
+	const handleClick = (idx) => {
+		audioRef.current.currentTime = Math.fround(idx * 20);
+	};
+
+	// placeholder example without timestamps
+	let audioSrc = audio;
+	let transcriptParagraphs = mapParagraphTag(transcript, handleClick);
+
+	return (
+		<>
+			<TranscriptView transcript={transcriptParagraphs} />
+			<AudioPlayer audioSrc={audioSrc} audioRef={audioRef} />
+		</>
+	);
+}
+
+export default AudioTranscript;
