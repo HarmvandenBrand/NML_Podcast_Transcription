af2c90366203389fb165e3e792bb0a65211472ae
EdwinWenink <edwinwenink@hotmail.com>
2020-04-13 15:36:37 +0200

    Create text refs en scroll to them on click

--- src/components/AudioTranscript.js
+++ src/components/AudioTranscript.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, createRef } from 'react';
 import { Container, Typography, IconButton } from '@material-ui/core';
 import Header from './Header';
 import { metadata, audio, transcript } from '../examplePodcast'; // example
@@ -26,13 +26,13 @@ function TranscriptView(props) {
  * @param {string} transcript the podcast transcript
  * @param {function} handleClick _onClick_ handler
  */
-function mapParagraphTag(transcript, handleClick) {
+function mapParagraphTag(transcript, handleClick, setTextRef) {
   let paragraphs = transcript.split(/\r?\n/).filter(Boolean);
   // index as key is bad practice in general but as the order of 
   // the paragraphs is fixed this is not an issue here
   return paragraphs.map((p, idx) =>
-    <p key={idx}>
-      <span onClick={() => handleClick(idx)}>
+    <p key={idx} id={'transcript_p' + idx}>
+      <span ref={ref => setTextRef(ref, idx)} onClick={() => handleClick(idx)}>
         {p}
       </span>
     </p>);
@@ -62,13 +62,20 @@ return (
 
 function AudioTranscript(props) {
   const audioRef = React.useRef(null);
+  const textRef = React.useRef(null);
+  const refsArray = useRef([]);
 
   const handleClick = (idx) => {
     audioRef.current.currentTime = Math.fround(idx * 20);
+    refsArray.current[idx].scrollIntoView();
   };
 
+  const setTextRef = (ref, idx) => {
+    refsArray.current[idx] = ref;
+  }
+
   // example with dummy timestamps
-  let transcriptParagraphs = mapParagraphTag(transcript, handleClick);
+  let transcriptParagraphs = mapParagraphTag(transcript, handleClick, setTextRef);
 
   return (
     <>
