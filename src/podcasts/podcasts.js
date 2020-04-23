28013ee8bfd0041de32a45464ad99d86f9dc19d4
EdwinWenink <edwinwenink@hotmail.com>
2020-04-23 17:09:05 +0200

    Remove test dependency

--- src/podcasts/podcasts.js
+++ src/podcasts/podcasts.js
@@ -4,11 +4,6 @@ const podcasts = {
 description: require("./salt_tectonics/description.json"),
 transcript: require("./salt_tectonics/transcript.json")
     },
-salt_tectonics_duplicate: {
-      audio: require("./salt_tectonics_duplicate/audio.mp3"),
-description: require("./salt_tectonics_duplicate/description.json"),
-transcript: require("./salt_tectonics_duplicate/transcript.json")
-    }
 }
 
 export default podcasts
