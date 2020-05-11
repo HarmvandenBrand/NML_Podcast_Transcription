1f9a87591c653d108fbdf2c44f18d9e1dfac675f
Harm <harmpie112@hotmail.com>
2020-05-11 23:40:28 +0200

    Added clickanimation for usability test. This branch is not to be merged into master.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import AudioTranscript from './components/AudioTranscript';
 import Navigation from './components/Navigation';
 import theme from './theme';
 import podcasts from './podcasts/podcasts.js';
+import './styles/ClickAnimation.css';
 
 function App() {
   const defaultEpisode = podcasts['IRL']['internet_carbon_footprint'] // temporary
   const [episode, setEpisode] = React.useState(defaultEpisode);
 
+  function clickEffect(e){
+      var d=document.createElement("div");
+      d.className="clickEffect";
+      d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
+      document.body.appendChild(d);
+      d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
+      console.log("clickeffect");
+    }
+    document.addEventListener('click',clickEffect);
 
   return (
     <ThemeProvider theme={theme}>
